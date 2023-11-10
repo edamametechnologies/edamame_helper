021eae37227e47d444aa6ba5e3084ceb380010a6
franklyonnet <flyonnet@edamame.tech>
2023-11-10 05:59:17 +0100

    Initial

--- macos/publish.sh
+++ macos/publish.sh
@@ -4,7 +4,7 @@ VERSION=$(grep '^version =' ../Cargo.toml | awk '{print $3}' | tr -d '"')
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Exception for main branch
-if [ "BRANCH" == "main" ]; then
+if [ "${BRANCH}" == "main" ]; then
   aws s3 cp ./target/edamame-helper.pkg s3://edamame-helper/macos/edamame-helper-"${VERSION}".pkg --acl public-read
 else
   aws s3 cp ./target/edamame-helper.pkg s3://edamame-helper/"${BRANCH}"/macos/edamame-helper-"${VERSION}".pkg --acl public-read
