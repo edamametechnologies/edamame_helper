2680300a3df9a854fe9f30c23647ed4281b95bae
Frank Lyonnet <flyonnet@edamame.tech>
2024-02-28 20:14:04 +0100

    UPDATE: secrets management update, udpdated edamame_foundation

--- build.rs
+++ build.rs
@@ -4,21 +4,26 @@ fn main() {
 
     // Dotenv build with a specific env path
     let config = dotenv_build::Config {
-        filename: std::path::Path::new("../edamame/secrets/foundation.env"),
+        filename: std::path::Path::new("../secrets/foundation.env"),
         recursive_search: false,
         fail_if_missing_dotenv: false,
         ..Default::default()
     };
-    dotenv_build::output(config).unwrap();
-
+    match dotenv_build::output(config) {
+        Ok(_) => (),
+        Err(e) => eprintln!("Error: {}", e),
+    }
     // Dotenv build with a specific env path
     let config = dotenv_build::Config {
-        filename: std::path::Path::new("../edamame/secrets/sentry.env"),
+        filename: std::path::Path::new("../secrets/sentry.env"),
         recursive_search: false,
         fail_if_missing_dotenv: false,
         ..Default::default()
     };
-    dotenv_build::output(config).unwrap();
+    match dotenv_build::output(config) {
+        Ok(_) => (),
+        Err(e) => eprintln!("Error: {}", e),
+    }
 
     // Emit the instructions
     let _ = EmitBuilder::builder().
