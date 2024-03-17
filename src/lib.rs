6afc65f182cff302390ccec5674484726969306a
Frank Lyonnet <flyonnet@edamame.tech>
2024-03-17 18:27:11 +0100

    FIX: improper TODO

--- src/lib.rs
+++ src/lib.rs
@@ -53,8 +53,6 @@ pub fn start_server() {
     async_exec(async {
         mdns_start().await
     });
-
-    // TODO: start a thread to regularly check on the network interfaces and flush the mDNS cache accordingly
     
     // Branch (needs to be gathered within the target executable's crate)
     let branch = envc!("VERGEN_GIT_BRANCH");
