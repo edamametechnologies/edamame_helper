274a8e2952c512c7293b7de397a3fd6ab6dd6380
Frank Lyonnet <flyonnet@edamame.tech>
2025-02-04 16:23:41 +0100

    Updated foundation

--- macos/uninstall.sh
+++ macos/uninstall.sh
@@ -6,19 +6,24 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 TARGET="/Library/Application Support/EDAMAME/EDAMAME-Helper"
+
 echo "Uninstalling EDAMAME-Helper"
 
 echo "Disabling EDAMAME-Helper"
 launchctl unload /Library/LaunchDaemons/com.edamametechnologies.edamame-helper.plist
+
 echo "Killing processes"
 killall edamame_helper
 sleep 10
 echo "Killing zombies"
 killall -9 edamame_helper
+
 # Warning !
-echo "Deleting installed files"
+echo "Deleting installed files and logs"
 rm -rf "$TARGET"
 rm /var/log/edamame_helper*
 rm /Library/LaunchDaemons/com.edamametechnologies.edamame-helper.plist
+
+pkgutil --forget com.edamametechnologies.edamame-helper
 
 echo "Done uninstalling EDAMAME-Helper"
