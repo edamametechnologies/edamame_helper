22a8a89e5853ec44acacba90bd02101fca5ba98f
Frank Lyonnet <flyonnet@edamame.tech>
2025-09-22 18:12:26 +0200

    Enhance Windows build process by copying necessary DLLs and updating main function to support debug assertions. The main function now starts the server in debug mode, while maintaining the original behavior for release builds.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ mod server;
 mod windows;
 #[cfg(not(target_os = "windows"))]
 use crate::server::start_server;
+#[cfg(all(target_os = "windows", debug_assertions))]
+use crate::server::start_server;
 use edamame_foundation::version::FOUNDATION_VERSION;
 use envcrypt::envc;
 
-#[cfg(target_os = "windows")]
+#[cfg(all(target_os = "windows", not(debug_assertions)))]
 fn main() -> windows_service::Result<()> {
     let url = envc!("EDAMAME_HELPER_SENTRY");
     // Needs to be gathered within the target executable's crate
@@ -23,6 +25,25 @@ fn main() -> windows_service::Result<()> {
         envc!("VERGEN_SYSINFO_USER")
     );
     windows::run(branch, url, release, &info_string)
+}
+
+#[cfg(all(target_os = "windows", debug_assertions))]
+fn main() {
+    let url = envc!("EDAMAME_HELPER_SENTRY");
+    // Needs to be gathered within the target executable's crate
+    let release = env!("CARGO_PKG_VERSION");
+    let branch = envc!("VERGEN_GIT_BRANCH");
+    let info_string = format!(
+        "Helper has version {}, is using Foundation version {} and has been built on {} with branch {} and signature {} on {} by {}",
+        env!("CARGO_PKG_VERSION"),
+        FOUNDATION_VERSION,
+        envc!("VERGEN_BUILD_TIMESTAMP"),
+        envc!("VERGEN_GIT_BRANCH"),
+        envc!("VERGEN_GIT_SHA"),
+        envc!("VERGEN_SYSINFO_OS_VERSION"),
+        envc!("VERGEN_SYSINFO_USER")
+    );
+    start_server(branch, url, release, &info_string);
 }
 
 #[cfg(not(target_os = "windows"))]
