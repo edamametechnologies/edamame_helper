9dd60be4d3bea44328d51cf83ab279efe51e807c
EDAMAME Dev Local <dev@edamame.tech>
2026-02-04 19:26:44 +0100

    Update edamame_helper to version 1.0.3 and adjust dependencies - Bump version of edamame_helper to 1.0.3 in Cargo.toml and chocolatey.nuspec. - Update installation scripts and package URLs to reflect the new version. - Modify dependencies in Cargo.lock, including downgrades and updates for various packages.

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'edamame-helper'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/edamametechnologies/edamame_helper/releases/download/v1.0.2/edamame-helper-windows-1.0.2.msi'
+$url64 = 'https://github.com/edamametechnologies/edamame_helper/releases/download/v1.0.3/edamame-helper-windows-1.0.3.msi'
 $checksum64 = '0000000000000000000000000000000000000000000000000000000000000000'
 
 Install-ChocolateyPackage -PackageName $packageName `
