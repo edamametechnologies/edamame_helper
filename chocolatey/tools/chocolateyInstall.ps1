91269303f33bd65efa235094b9a1eae8b353eca4
EDAMAME Dev Local <dev@edamame.tech>
2026-05-22 13:38:12 +0200

    Pre-publish

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'edamame-helper'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/edamametechnologies/edamame_helper/releases/download/v1.3.14/edamame-helper-windows-1.3.14.msi'
+$url64 = 'https://github.com/edamametechnologies/edamame_helper/releases/download/v1.3.15/edamame-helper-windows-1.3.15.msi'
 $checksum64 = '0000000000000000000000000000000000000000000000000000000000000000'
 
 Install-ChocolateyPackage -PackageName $packageName `
